31ffbb27bc9cb61e0634e671498b28d8080de3e6
Manuel <manuel@guerrerocabrera.com>
2023-12-27 10:23:12 +0100

    Cambio del resto de rutas de imágenes

--- src/components/OptionsHeader/OptionsHeader.js
+++ src/components/OptionsHeader/OptionsHeader.js
@@ -6,15 +6,15 @@ import "./OptionsHeader.css";
 // Objeto con las url de las imágenes y los alt
 const imgData = [
     {
-        url: "./public/bell-icon.png",
+        url: "../../../public/bell-icon.png",
         alt: "icono avisos"
     },
     {
-        url: "./public/dialog-icon.png",
+        url: "../../../public/dialog-icon.png",
         alt: "icono chat"
     },
     {
-        url: "./public/usuario.png",
+        url: "../../../public/usuario.png",
         alt: "icono perfil usuario"
     }
 ];
