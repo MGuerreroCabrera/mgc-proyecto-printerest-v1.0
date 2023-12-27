d67f5ff8139618d4ed362ffd910d27be3e44d9f9
Manuel <manuel@guerrerocabrera.com>
2023-12-27 22:51:28 +0100

    Más

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,7 +13,7 @@ export const printHeader = () => {
 
     // Añadir logotipo
     const logoHTML = document.createElement("img");
-    logoHTML.src = "./assets/logo-phinterest.png";
+    logoHTML.src = "/assets/logo-phinterest.png";
     logoHTML.alt = "Logo phinterest";
     logoHTML.id = "logoPhinterest";
     headerHTML.appendChild(logoHTML);
